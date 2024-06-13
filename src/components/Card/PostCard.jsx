a4325e7c414b9bf9759b2113fca58ede7ff74798
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-13 15:13:53 +0200

    added posts cards with properties

--- src/components/Card/PostCard.jsx
+++ src/components/Card/PostCard.jsx
@@ -1,27 +1,44 @@
-// Importo lo style della card ed il button
+// Importo lo style della card, il Button ed il ListItem
 import postCardStyle from './PostCard.module.css';
 import Button from '../Button/Button';
+import ListItem from '../ListItem/ListItem';
 
-const PostCard = () => {
+const PostCard = ({ p }) => {
     return (
         <>
             {/* Post Card */}
-            <div className={postCardStyle.card}>
+            <div className={`${postCardStyle.card} ${p.published ? 'published' : postCardStyle.notPublished}`}>
                 {/* Post Image */}
                 <div className={postCardStyle.image}>
-                    <img src='' alt='' className={postCardStyle.img} />
+                    <img src={p.image ? p.image : "https://placehold.co/600x400"} alt={p.title} className={postCardStyle.img} />
                 </div>
                 <div className={postCardStyle.bottom}>
                     {/* Post Title */}
-                    <h3>Titolo del Post</h3>
+                    <h3>{p.title}</h3>
                     {/* Post Content */}
                     <p className={postCardStyle.paragraph}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur possimus cumque voluptatibus, quo aliquam amet dolorum obcaecati harum voluptas dicta adipisci accusantium culpa sequi esse, quos eos ratione fuga est!
+                        {p.content}
                     </p>
-                    {/* Button */}
-                    <Button />
+                    <div className={postCardStyle.dFlex}>
+                        {/* Button */}
+                        <Button />
+                        {p.tags.length > 0
+                            ?
+                            <div>
+                                <ul className={postCardStyle.tags}>
+                                    {/* Tags */}
+                                    {p.tags.map((t) => (
+                                        // List Item
+                                        <ListItem key={t.id} t={t} />
+                                    ))}
+                                </ul>
+                            </div>
+                            :
+                            'Nessun tag'
+                        }
+                    </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
